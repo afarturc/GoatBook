3c03d32d65e3322f3399f1e6a34014ac19bda78a
mariana <marianaandrade@ua.pt>
2022-11-14 19:22:45 +0000

    search

--- TPW_Project1/settings.py
+++ TPW_Project1/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-f3-!_21b(i^nlahu00v$66fwnmg!_bg=33#k%c4twhta@e4yek"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
